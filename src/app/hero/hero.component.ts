aa449952287346c008045544546f664f39c06dad
HamzaBnJemaa <benjemaahamza09@gmail.com>
2025-11-07 02:53:50 +0100

    why choose mentor upgrade & course details hamza

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { WhyChooseMentorComponent } from "../why-choose-mentor/why-choose-mentor.component"
+import { CoursesComponent } from "../courses/courses.component"
 
 @Component({
   selector: "app-hero",
   standalone: true,
-  imports: [CommonModule, WhyChooseMentorComponent],
+  imports: [CommonModule, WhyChooseMentorComponent, CoursesComponent],
   templateUrl: "./hero.component.html",
   styleUrls: ["./hero.component.css"],
 })
